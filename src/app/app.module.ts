ebde9170d0b7fb131fc929a5deed0525d4d4dd1e
KatherineRetamales <kretamales280@gmail.com>
2019-05-22 12:41:11 -0400

    Se creo un nuevo componente para las rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { PrincipalComponent } from './principal/principal.component';
+import { RutaComponent } from './ruta/ruta.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, MenuComponent, PrincipalComponent],
+  declarations: [AppComponent, HomeComponent, MenuComponent, PrincipalComponent, RutaComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
