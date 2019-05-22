a32b69143751974ebf4f5ed78fc8e7bd547ed8b5
ScarlettBurboa <scarlett.burboa@gmail.com>
2019-05-22 14:41:07 -0400

    Eliminando mapas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component,OnInit, ViewChild, ElementRef } from '@angular/core';
-
-declare var H: any;
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,31 +6,10 @@ declare var H: any;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
   private platform: any;
 
-  @ViewChild("map")
+  @ViewChild('map')
   public mapElement: ElementRef;
 
-  public constructor() {
-      this.platform = new H.service.Platform({
-          "app_id": "5FcjVZI69NvLSO0NNyiw",
-          "app_code": "n3Tvm27JNHALxUlmHwptWQ"
-      });
-  }
-
-  public ngOnInit() { }
-
-  public ngAfterViewInit() {
-      let defaultLayers = this.platform.createDefaultLayers();
-      let map = new H.Map(
-          this.mapElement.nativeElement,
-          defaultLayers.normal.map,
-          {
-              zoom: 100,
-              center: { lat: 37.7397, lng: -121.4252 }
-          }
-      );
-  }
-
+  public ngOnInit() {}
 }
